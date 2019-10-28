45efe6bffe8277e4f22e1c8bb9130f49d9a2dd53
beauhodes <beauhodes@gmail.com>
2019-10-28 11:26:38 -0500

    Began pikachu file

---
+++ pikachu.py
@@ -0,0 +1,14 @@
+import pokebase as pb
+
+class Pikachu:
+    def __init__(self, etc):
+        """
+        Initialize arguments
+        Args:
+        size - size of boat tile
+        coordinates - array of coordinates
+        Returns:
+        None
+        """
+        pokeBasePikachu = pb.APIResource('pikachu')
+        #continue to add features
